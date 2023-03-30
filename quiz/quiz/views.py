dfbf75100d143fbce8bf2ffb6d610cb0efc5596f
champaksworldcreate <champaksworld@gmail.com>
2023-03-30 14:00:07 +0530

    Questions coming now

--- quiz/quiz/views.py
+++ quiz/quiz/views.py
@@ -4,7 +4,24 @@ from . import ProgrammingQuestions as questionsbank
 
 
 def quiz(request):
-    currentquizno = 0
-    currentquestion = questionsbank.questions[currentquizno]
-    return render(request, "quiz.html")
+    correctresult='<span class="material-icons" style="color: green">check</span>'
+    wrongresult='<span class="material-icons" style="color: red">close</span>'
+    currentquestionno = -1
+    currentquestion = 'Press any key to start'
+    questions = questionsbank.questions
+    n = len(questions)
+
+    if request.GET:
+        currentquestionno = int(request.GET["questionno"])
+        print(currentquestionno)
+        currentquestionno += 1
+        # currentquestion = questionsbank.questions[currentquizno]
+
+        if currentquestionno >= n:
+            currentquestion = 'Test over'
+        else:
+            if currentquestionno == -1:
+                currentquestionno += 1
+            currentquestion = questions[currentquestionno]
+    return render(request, "quiz.html", {'qno': currentquestionno, "question": currentquestion,"results":correctresult})
     # return HttpResponse("Home")
